7df9d9b0e873382871b7e4c57632e399ebf71aa7
Pierce Darragh <pierce.darragh@gmail.com>
2020-11-21 16:52:58 -0700

    Configure Scribble documentation to be built into main docs

--- varcur-calc/info.rkt
+++ varcur-calc/info.rkt
@@ -7,7 +7,7 @@
 (define pkg-desc "A small, untyped lambda calculus extended to support both automatic currying and variadic functions.")
 (define pkg-authors '((@author+email "Pierce Darragh" "pierce.darragh@gmail.com")))
 
-(define scribblings '(("scribblings/varcur-calc.scrbl")))
+(define scribblings '(("scribblings/varcur-calc.scrbl" (multi-page main-doc) (library))))
 
 (define deps '("base"))
 (define build-deps '("scribble-lib"
