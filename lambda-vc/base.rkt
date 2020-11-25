99f9e7d36b7aa70f7bcd24d03c0f386fdcef383b
Pierce Darragh <pierce.darragh@gmail.com>
2020-11-25 16:58:50 -0700

    Add comments for environment functions

--- lambda-vc/base.rkt
+++ lambda-vc/base.rkt
@@ -45,10 +45,22 @@
 ;;
 ;; Environments
 ;;
+;; Environments in lambda-vc are just lists of pairs:
+;;   (list ... (cons variable-name variable-value) ...)
+;; To extend an environment requires only to cons a new pair onto the front of
+;; the list. This allows us to avoid many non-pathological cases of hygiene.
 
+;; The empty environment is just an empty list.
 (define mt-env empty)
+(define env-empty? empty?)
+;; Extending an environment requires consing a new pair on the head of the
+;; current environment.
 (define (extend-env name value env)
   (cons (cons name value) env))
+;; We look up variables' values in the environment by performing an in-order
+;; search for the variable name. By doing it this way, inner variables that are
+;; identical to outer variables simply shadow, avoiding difficulties introduced
+;; by other lookup schemes.
 (define (env-lookup env name)
   (match env
     [(list) #f]
@@ -56,13 +68,12 @@
      (if (equal? name l-name)
          l-value
          (env-lookup rest-env name))]))
-(define env-empty? empty?)
 
+;; We provide a simple prelude to support basic arithmetic by making external
+;; calls to Racket.
 (define prelude
   (foldl (λ (pair env)
-           (extend-env (car pair)
-                       (cdr pair)
-                       env))
+           (cons pair env))
          mt-env
          (list
           (cons '+ (ffi-closure +))
