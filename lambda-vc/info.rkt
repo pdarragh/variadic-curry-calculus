363d7d21a6c999f2a87c7d596410ea0e24da0bb5
Pierce Darragh <pierce.darragh@gmail.com>
2020-11-28 02:35:39 -0700

    Disable multi-page mode

--- lambda-vc/info.rkt
+++ lambda-vc/info.rkt
@@ -7,7 +7,7 @@
 (define pkg-desc "A small, untyped lambda calculus extended to support both automatic currying and variadic functions.")
 (define pkg-authors '((@author+email "Pierce Darragh" "pierce.darragh@gmail.com")))
 
-(define scribblings '(("scribblings/lambda-vc.scrbl" (multi-page main-doc) (library))))
+(define scribblings '(("scribblings/lambda-vc.scrbl" (main-doc) (library))))
 
 (define deps '("base"
                "scribble-math"))
