e76165119feefc0604ddab24d37c5039efb643a4
Pierce Darragh <pdarragh@Pierces-MacBook-Pro.local>
2021-01-17 14:07:40 -0700

    Commit work on in-progress small-step interpreter

--- lambda-vc/small-step.rkt
+++ lambda-vc/small-step.rkt
@@ -1,81 +1,91 @@
-(require "lambda-vc.rkt")
+#lang racket
 
 (provide (all-defined-out))
 
-(struct closure ...)
-(struct closure-value ...)
+(require "env.rkt")
 
-(struct function (formals body) #:transparent)
+;; All "values" are sub-types of the value struct. This way, we can use the
+;; predicate `value?` to determine all values.
+(struct value ())
 
-(define (value? x)
-  (or (integer? x)
-      (closure-value? x)
-      ...))
+;; Error messages are wrapped in structs so superposition collapsing can avoid
+;; installing error handlers.
+(struct err-value value (message) #:transparent)
 
-(define (valid-formal? formal)
-  (and (symbol? formal)
-       (regexp-match #px"^[[:alpha:]](-[[:word:]])*$"
-                     (symbol->string formal))))
+;; TODO
+(struct superposition-value value (lhs rhs))
 
-(define (parse exp)
+;; Superpositions are just two grouped inner expressions.
+(struct superposition (lhs rhs)
+  #:methods gen:custom-write
+  [(define (write-proc s port mode)
+     (match s
+       [(superposition lv rv)
+        (write-string (format "(σ ~a ~a)" lv rv)
+                      port)]))])
+
+;; Attempts to perform a single-step reduction of `exp`. Returns a pair:
+;;
+;; 1. The returned expression.
+;; 2. `#f` if the returned expression is identical to the input expression
+;;    (i.e., if no reduction occurred), or else a symbol naming the rule
+;;    responsible for the reduction.
+;;
+;; NOTE: If multiple rules are applied in one step, the rules are returned as a
+;;       list with the inner-most rules to the end. (This is still a "small
+;;       step" since only a single reduction is performed.)
+(define (step exp env)
+  (define (return-change new-exp rule)
+    (values new-exp rule))
+  (define (return-no-change)
+    (values exp #f))
+  (define (recur-step sub-exp change-func outer-rule)
+    ;; This should only be called when we expect sub-exp to reduce.
+    (let-values ([(sub-exp-result inner-rule) (step sub-exp env)])
+      (return-change
+       (change-func sub-exp-result)
+       (cons outer-rule inner-rule))))
   (match exp
-    [(? value?) exp]
+    ;; VALUES
+    [(? value?)
+     (return-no-change)]
+    ;; SYMBOLS
     [(? symbol?)
-     (if (valid-formal? exp)
-         ...
-         ...)]
-    [`(,(or 'λ 'lambda) (,formals ...) ,body)
+     ;; Attempt to look up symbols in the environment. If no binding exists,
+     ;; it's an error. There is always something to reduce here.
+     (let ([lookup (env-lookup env exp)])
+       (if lookup
+           (return-change lookup
+                          'E-Substitute)
+           (return-change (err-value (format "free variable: ~a" exp))
+                          'Err-Substitute)))]
+    ;; SUPERPOSITIONS
+    [(superposition lhs rhs)
+     (match (cons lhs rhs)
+       [(cons (? err-value?) (? err-value?))
+        ;; Reduction results in an error either way.
+        (return-change
+         (err-value "erroneous superposition")
+         'Err-Superposition)]
+       [(cons (? err-value?) _)
+        (return-change rhs 'E-SuperpositionErrorReduce1)]
+       [(cons _ (? err-value?))
+        (return-change lhs 'E-SuperpositionErrorReduce2)]
+       [(cons (? value?) _)
+        (recur-step
+         rhs
+         (λ (new-rhs)
+           (if (value? new-rhs)
+               (superposition-value lhs new-rhs)
+               (superposition lhs new-rhs)))
+         'E-SuperpositionReduce2)]
+       [_
+        (recur-step
+         lhs
+         (λ (new-lhs) (superposition new-lhs rhs))
+         'E-SuperpositionReduce1)])]
+    ;; FUNCTIONS
+    [`(,(or 'λ 'λ ) (,formals ...) ,body)
      (match formals
        [(list)
-        ...]
-       [(list other-formals '...)
-        ...]
-       [else
-        ...])
-     (function (map parse formals) (parse body))]))
-
-(define (small-step exp env)
-  (match exp
-    [(? closure?)
-     (? )]
-    [(? symbol?)
-     ;; RULE: E-Substitute
-     (or (env-lookup env exp)
-         (err (format "interpretation of free variable: ~a" exp)))]
-    [`(,(or `λ `lambda) (,formals ...) ,body)
-     (match formals
-       [(list)
-        ;; A nullary (zero-argument) function.
-        (closure env #f body)]
-       [(list formal formals ...)
-        (if (not (valid-formal? formal))
-            (err (format "invalid formal parameter name: ~a" formal))
-            (match formals
-              [(list)
-               ;; A unary (one-argument) function.
-               (closure env (param formal) body)]
-              [(list '...)
-               ;; Variadic functions are defined by supplying a single formal
-               ;; parameter with a trailing ellipsis.
-               ;;
-               ;; We only allow variadic functions to be defined within the
-               ;; body of another function, because the outer function's formal
-               ;; parameter will serve as the point at which to inject the
-               ;; superpositional return value in subsequent applications of
-               ;; the variadic function.
-               ;;
-               ;; TODO: At the moment, this check involves looking at the
-               ;;       environment to determine if it's either empty or has an
-               ;;       FFI function in the most-recently-bound position. This
-               ;;       is fragile, and an alternative encoding of environments
-               ;;       that separates the user's environment from the prelude
-               ;;       would be preferred.
-               (if (or (env-empty? env)
-                       (ffi-closure? (cdar env)))
-                   ;; The environment doesn't support a variadic function.
-                   (err "variadic functions may only be defined within the body of another function")
-                   ;; We can build a variadic function, so do it!
-                   (variadic-closure env (param formal) body))]
-              [else
-               ;; RULE: E-Curry
-               (closure env (param formal) `(λ ,formals ,body))]))])]))
+        #f])]))  ;; FIXME
