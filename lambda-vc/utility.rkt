dc49dd865185bea7aa421957a464789a2a86d33d
Pierce Darragh <pierce.darragh@gmail.com>
2021-01-20 18:55:06 -0700

    Remove definition of findf+index

--- lambda-vc/utility.rkt
+++ lambda-vc/utility.rkt
@@ -12,19 +12,6 @@
        (regexp-match #px"^[[:alpha:]](-[[:word:]])*$"
                      (symbol->string formal))))
 
-;; Like findf, but instead of only returning the found item (or #f) this version
-;; returns the item and the index at which it was found (or #f if not found).
-(define (findf+index pred lst)
-  (define (findf+index lst index)
-    (match lst
-      [(list)
-       #f]
-      [(list item rest-lst ...)
-       (if (pred item)
-           (cons item index)
-           (findf+index rest-lst (add1 index)))]))
-  (findf+index lst 0))
-
 ;; A mix of member and partition. Given a predicate and a list, finds the first
 ;; element of the list that satisfies the predicate. If found, a triple is
 ;; returned consisting of the list prior to the element, the element, and the
